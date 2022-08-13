3b310badb489bb2fd24df6d4841cb863dfa91226
Aditya Kumar Singh <sadityakumar9211@gmail.com>
2022-08-13 10:37:35 +0530

    minor ui changes

--- components/Header.js
+++ components/Header.js
@@ -72,14 +72,6 @@ export default function Header() {
                         alt="overlapped square svg"
                     />
                 </div>
-
-                <div
-                    id="nav-mobile-btn"
-                    className="absolute top-0 right-0 z-50 block w-6 mt-8 mr-10 cursor-pointer select-none md:hidden sm:mt-10"
-                >
-                    <span className="block w-full h-1 mt-2 duration-200 transform bg-gray-800 rounded-full sm:mt-1"></span>
-                    <span className="block w-full h-1 mt-1 duration-200 transform bg-gray-800 rounded-full"></span>
-                </div>
             </div>
         </header>
     )
